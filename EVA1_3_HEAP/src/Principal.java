a754ebee9883534324d105b2c5143800b6a23c1a
ChaoticErnie <51009931+ChaoticErnie@users.noreply.github.com>
2019-10-06 11:17:39 -0600

    Add files via upload

---
+++ EVA1_3_HEAP/src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        Principal pObj = new Principal();
+        System.out.println(i);
+        System.out.println(pObj);
+
+        pObj = null; //Libera memoria
+
+    }
+
+}
